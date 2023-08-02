8d7309429235fd7e9b40c28ceea3f7ec7cbf7674
Aniol Vergés Herrera <aniol.verges@students.salle.url.edu>
2023-08-02 08:42:44 +0200

    Ja es guarden!!!

--- script.js
+++ script.js
@@ -3,22 +3,37 @@ src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
 src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
 src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
 
-document.addEventListener('DOMContentLoaded', function() {
 
+
+
+document.addEventListener('DOMContentLoaded', function() {
     class Task {
         constructor(name, description) {
             this.name = name;
             this.description = description;
-            this.checked = false;
         }
     }
 
-    const llistaTODO = [];
+    let llistaTODO = [];
+    let numTasksCompleted = 0;
+    const dadesGuardades = localStorage.getItem('dades');
+    const numTasksGuardades = localStorage.getItem('num');
+    if(dadesGuardades){
+        llistaTODO = JSON.parse(dadesGuardades);
+    }
+    if(numTasksGuardades){
+        numTasksCompleted = JSON.parse(numTasksGuardades);
+    }
+
     const container = document.getElementById("container");
+    mostrarTasques();
 
     function mostrarTasques() {
+
         container.innerHTML = '';
         const textInput = document.getElementById("newTaskText");
+        const numTasksHTML = document.getElementById("numTasks");
+        numTasksHTML.textContent = numTasksCompleted.toString();
         textInput.value = '';
 
         for (let i = 0; i < llistaTODO.length; i++) {
@@ -45,8 +60,7 @@ document.addEventListener('DOMContentLoaded', function() {
                                             <button type="button" id="taskDone${i}" class="btn btn-sm btn-success waves-effect waves-light"><i class="bi bi-check-lg"></i></button>
                                         </div>
                                         <div class="col-sm-2 text-end">
-                                            <input type="checkbox" class="btn-check" id="btn-check-delete${i}" autocomplete="off">
-                                            <label class="btn btn-sm btn-outline-danger" for="btn-check-delete${i}"><i class="bi bi-x-lg"></i></label>
+                                            <button type="button" id="deleteTask${i}" class="btn btn-sm btn-danger waves-effect waves-light"><i class="bi bi-x-lg"></i></button>
                                         </div>
                                     </div>
                                 </div>
@@ -56,6 +70,12 @@ document.addEventListener('DOMContentLoaded', function() {
             taskDoneButton.addEventListener('click', () => {
                 taskDone(i);
             });
+
+            const taskDeletedButon = taskCard.querySelector(`#deleteTask${i}`);
+            taskDeletedButon.addEventListener('click', () => {
+                deleteTask(i);
+            });
+
 
             taskCard.querySelector(`#taskDone${i}`).addEventListener("click", function (e) {
                 party.confetti(this);
@@ -67,12 +87,31 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
     }
+
     function taskDone(i){
         const targetCard = document.querySelector(`#taskCard${i}`);
+        targetCard.style.animation = "fade-left 0.5s";
         if (targetCard) {
-            targetCard.style.animation = "fade-left 1s";
             targetCard.addEventListener("animationend", function() {
                 llistaTODO.splice(i, 1);
+                const jsonString = JSON.stringify(llistaTODO);
+                localStorage.setItem('dades', jsonString);
+                numTasksCompleted++;
+                localStorage.setItem('num',numTasksCompleted.toString());
+                mostrarTasques();
+            });
+        }
+    }
+
+    function deleteTask(i){
+        const targetCard = document.querySelector(`#taskCard${i}`);
+
+        targetCard.style.animation = "smaller 0.5s";
+        if(targetCard){
+            targetCard.addEventListener("animationend", function() {
+                llistaTODO.splice(i, 1);
+                const jsonString = JSON.stringify(llistaTODO);
+                localStorage.setItem('dades', jsonString);
                 mostrarTasques();
             });
         }
@@ -91,6 +130,8 @@ document.addEventListener('DOMContentLoaded', function() {
             }, 2000);
         }else{
             llistaTODO.push(new Task(text, ""));
+            const jsonString = JSON.stringify(llistaTODO);
+            localStorage.setItem('dades', jsonString);
             mostrarTasques();
         }
     }
